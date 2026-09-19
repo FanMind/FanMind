ee2aff51885a0d6b2adeceb4aae31588adae26bc
Bernds-tech <b.guggennberger@gmail.com>
2026-09-19 13:28:33 +0200

    feat: expose Facebook comment sync from channels

--- src/app/channels/metaSyncActions.ts
+++ src/app/channels/metaSyncActions.ts
@@ -1,12 +1,20 @@
 "use server";
 
-import { syncFacebookMessengerHistory } from "./facebookWebhookActions";
+import {
+  fetchFacebookCommentsNow,
+  syncFacebookMessengerHistory,
+} from "./facebookWebhookActions";
 import { syncInstagramMessengerHistory } from "./instagramWebhookActions";
 import { requireActiveAuthorizedWorkspace } from "@/lib/workspaceAuthorization";
 
 export async function syncFacebookMessengerHistoryFromChannelPage(): Promise<void> {
   await requireActiveAuthorizedWorkspace();
   await syncFacebookMessengerHistory({ revalidate: true });
+}
+
+export async function syncFacebookCommentsFromChannelPage(): Promise<void> {
+  await requireActiveAuthorizedWorkspace();
+  await fetchFacebookCommentsNow();
 }
 
 export async function syncInstagramMessengerHistoryFromChannelPage(): Promise<void> {
